a107d714f344cb73bb367db20712c55fa506cd93
awayy1432 <awayy1432@163.com>
2014-04-08 15:40:37 +0800

    web相关、xml处理

---
+++ textop/xmlop.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type Servers struct {
+	XMLName     xml.Name `xml:"servers"`
+	Version     string   `xml:"version,attr"`
+	Svs         []server `xml:"server"`
+	Description string   `xml:",innerxml"`
+}
+
+type server struct {
+	ServerName string `xml:"serverName"`
+	ServerIp   string `xml:"serverIP"`
+}
+
+func main() {
+	/******* 写文件 ******/
+	// v := &Servers{Version: "1"}
+	// v.Svs = append(v.Svs, server{"ShangHai", "127.0.0.1"})
+	// v.Svs = append(v.Svs, server{"BieJing", "127.0.0.2"})
+	// output, err := xml.MarshalIndent(v, "\t", "\t")
+	// if err != nil {
+	// 	fmt.Printf("error:%v", err)
+	// }
+
+	// fout, err := os.Create("Server.xml")
+	// defer fout.Close()
+	// if err != nil {
+	// 	fmt.Println("Server.xml create fail:", err)
+	// 	return
+	// }
+
+	// fout.Write([]byte(xml.Header))
+	// fout.Write(output)
+
+	/******* 读文件 ******/
+
+	fin, err := os.Open("Server.xml")
+
+	if err != nil {
+		fmt.Println("Server.xml read fail:", err)
+		return
+	}
+	defer fin.Close()
+
+	data, err := ioutil.ReadAll(fin)
+	if err != nil {
+		fmt.Println("Server.xml read fail:", err)
+		return
+	}
+	v := Servers{}
+	err = xml.Unmarshal(data, &v)
+	if err != nil {
+		fmt.Println("Server.xml read fail:", err)
+		return
+	}
+
+	fmt.Println(v)
+}
---
+++ web/mymux.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type MyMux struct {
+}
+
+func (m *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		sayhello(w, r)
+		return
+	}
+	http.NotFound(w, r)
+	return
+}
+
+func sayhello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello,MyMux")
+}
+
+func main() {
+	mux := &MyMux{}
+	http.ListenAndServe(":8899", mux)
+}
