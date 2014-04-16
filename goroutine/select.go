0d23359ca9d063d890a6417bb2e366fa9ed1816b
awayy1432 <awayy1432@163.com>
2014-04-16 09:54:52 +0800

    select

--- goroutine/select.go
+++ goroutine/select.go
@@ -7,6 +7,7 @@ import (
 
 func dataIn(c chan int, d chan int) {
 	timer := time.NewTicker(3 * time.Second)
+	i := 0
 	for {
 		select {
 		case i := <-c:
@@ -14,19 +15,43 @@ func dataIn(c chan int, d chan int) {
 		// case <-time.After(time.Second * 5):
 		// 	fmt.Println(time.Now().String())
 		// 	d <- 10
-		// }
 		case <-timer.C:
 			fmt.Println(time.Now().String())
-			d <- 10
+			i++
+			d <- i
 		}
 	}
 }
 
-func main() {
+func test1() {
 	c := make(chan int)
 	d := make(chan int)
 	go dataIn(c, d)
 	for {
-		<-d
+		if j := <-d; j > 10 {
+			break
+		}
 	}
+}
+
+func dataIn1(c, d chan int) {
+	select {
+	case <-c:
+		fmt.Println("cccc")
+		d <- 10
+	}
+}
+
+//死锁
+func test2() {
+	c := make(chan int)
+	d := make(chan int)
+	go dataIn1(c, d)
+	<-d
+	close(c)
+
+}
+
+func main() {
+	test1()
 }
