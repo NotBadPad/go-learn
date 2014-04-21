f3b7fcb4d0c011779779e63728d880201f4e45e3
awayy1432 <awayy1432@163.com>
2014-04-21 19:28:37 +0800

    rand

--- base/var/main.go
+++ base/var/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
 	"strconv"
 )
 
-func main() {
+func test1() {
 	s1 := "12"
 	strconv.ParseInt(s1, 10, 32)
 	fmt.Println(s1)
+}
+
+func test2() {
+	alphanum := "0123456789"
+	var bytes = make([]byte, 3)
+	rand.Read(bytes)
+	for i, b := range bytes {
+		bytes[i] = alphanum[b%byte(len(alphanum))]
+		fmt.Println(b % byte(len(alphanum)))
+	}
+
+	fmt.Println(string(bytes))
+}
+
+func main() {
+	test2()
 }
