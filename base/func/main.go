4952e8794bb6a61db6d3010cbe9f1d5b9d12ca3a
awayy1432 <awayy1432@163.com>
2014-05-07 19:50:44 +0800

    base

--- base/func/main.go
+++ base/func/main.go
@@ -5,11 +5,26 @@ import (
 	"os"
 )
 
-func main() {
+func test1() {
 	encryptStr := os.Args[1]
 	if encryptStr == "" {
 		fmt.Errorf("Error s%", "参数为空")
 		return
 	}
 	fmt.Println(encryptStr)
+}
+
+func test2() {
+	type Param struct {
+		Id   int64
+		Name string
+	}
+	a := Param{Id: 12, Name: "guojing"}
+	fmt.Printf("%s\n", a)
+	fmt.Printf("%+s\n", a)
+	fmt.Printf("%#s\n", a)
+}
+
+func main() {
+	test2()
 }
