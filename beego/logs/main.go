34d6fe37bf044c320b9e878a2ea0004953f5e4ff
awayy1432 <awayy1432@163.com>
2014-05-22 18:54:18 +0800

    beego

--- beego/logs/main.go
+++ beego/logs/main.go
@@ -5,9 +5,27 @@ import (
 	"github.com/astaxie/beego/httplib"
 )
 
+// 响应数据结构
+type ApiResp struct {
+	Head ApiHead     `json:"head"`
+	Body interface{} `json:"body,omitempty"`
+}
+
+// 响应头结构
+type ApiHead struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+}
+
 func test2() {
-	req := httplib.Get("http://www.baidu.com/")
-	fmt.Println(req.Response().StatusCode)
+	req := httplib.Get("http://localhost:8081/test")
+	httpRes, _ := req.Response()
+	fmt.Println(httpRes.StatusCode)
+
+	result := &ApiResp{}
+	err := req.ToJson(result)
+	fmt.Println(err)
+	fmt.Println(result)
 }
 
 func main() {
