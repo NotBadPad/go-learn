fb012794651f49e3a5fd37ebc575ed955ab4b52e
awayy1432@163.com <awayy1432@163.com>
2014-06-29 22:57:24 +0800

    template

--- textop/template.go
+++ textop/template.go
@@ -7,26 +7,40 @@ import (
 
 type Person struct {
 	UserName string
-	Emails   map[string]string
+	Emails   map[string]interface{}
 	Names    []string
 }
 
-func main() {
+func test1() {
+	// 	t := template.New("test")
+	// 	t, _ = t.Parse(`
+	// {{.UserName}}
+	// {{.Emails.v1}}
+	// 		`)
+	// 	user := Person{"guojing", make(map[string]string, 0), make([]string, 3)}
+	// 	user.Emails["v1"] = "test1"
+	// 	user.Emails["v2"] = "test2"
+	// 	user.Names[0] = "aaaaa"
+	// 	t.Execute(os.Stdout, user)
+}
+
+func test2() {
 	t := template.New("test")
 	t, _ = t.Parse(`
-
-	 {{range $k,$v := .Emails}}
-
-	 {{$k}}:{{$v}},
-
-	 {{end}}
-		{{with .Names}} {{}} {{end}}
-
-
+{{.UserName}}
+{{.Emails.v2.b}}
+{{range .Emails.v3}}
+	{{.}}
+{{end}}
 		`)
-	user := Person{"guojing", make(map[string]string, 0), make([]string, 3)}
+	user := Person{"guojing", make(map[string]interface{}, 0), make([]string, 3)}
 	user.Emails["v1"] = "test1"
-	user.Emails["v2"] = "test2"
+	user.Emails["v2"] = map[string]string{"a": "4321", "b": "123"}
+	user.Emails["v3"] = []int{1, 3, 4, 2, 3}
 	user.Names[0] = "aaaaa"
 	t.Execute(os.Stdout, user)
+}
+
+func main() {
+	test2()
 }
