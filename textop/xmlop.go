3a4d65ada1be726a4fc6f76d2dac5aaf2e304328
awayy1432@163.com <awayy1432@163.com>
2014-07-21 21:46:39 +0800

    textop

--- textop/xmlop.go
+++ textop/xmlop.go
@@ -19,26 +19,7 @@ type server struct {
 	ServerIp   string `xml:"serverIP"`
 }
 
-func main() {
-	/******* 写文件 ******/
-	// v := &Servers{Version: "1"}
-	// v.Svs = append(v.Svs, server{"ShangHai", "127.0.0.1"})
-	// v.Svs = append(v.Svs, server{"BieJing", "127.0.0.2"})
-	// output, err := xml.MarshalIndent(v, "\t", "\t")
-	// if err != nil {
-	// 	fmt.Printf("error:%v", err)
-	// }
-
-	// fout, err := os.Create("Server.xml")
-	// defer fout.Close()
-	// if err != nil {
-	// 	fmt.Println("Server.xml create fail:", err)
-	// 	return
-	// }
-
-	// fout.Write([]byte(xml.Header))
-	// fout.Write(output)
-
+func test1() {
 	/******* 读文件 ******/
 
 	fin, err := os.Open("Server.xml")
@@ -62,4 +43,30 @@ func main() {
 	}
 
 	fmt.Println(v)
+}
+
+func test2() {
+	/******* 写文件 ******/
+	// v := &Servers{Version: "1"}
+	// v.Svs = append(v.Svs, server{"ShangHai", "127.0.0.1"})
+	// v.Svs = append(v.Svs, server{"BieJing", "127.0.0.2"})
+	// output, err := xml.MarshalIndent(v, "\t", "\t")
+	// if err != nil {
+	// 	fmt.Printf("error:%v", err)
+	// }
+
+	// fout, err := os.Create("Server.xml")
+	// defer fout.Close()
+	// if err != nil {
+	// 	fmt.Println("Server.xml create fail:", err)
+	// 	return
+	// }
+
+	// fout.Write([]byte(xml.Header))
+	// fout.Write(output)
+
+}
+
+func main() {
+	test1()
 }
