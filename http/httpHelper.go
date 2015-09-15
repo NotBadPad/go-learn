4d5d319db11df62ece8a89eb717f83230972be02
awayy1432 <awayy1432@163.com>
2015-09-15 18:50:53 +0800

    完善httpHelper

--- http/httpHelper.go
+++ http/httpHelper.go
@@ -3,7 +3,15 @@ package components
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/httplib"
+	"time"
+)
+
+const (
+	CONNECT_TIME_OUT    = 100 * time.Second //连接超时时间
+	READ_WRITE_TIME_OUT = 30 * time.Second  //读写超时时间
 )
 
 // 响应数据结构
@@ -25,16 +33,104 @@ type ApiHead struct {
  * 	data：body数据对象接口
  */
 func GetHttpRequest(url string, data interface{}) (code int, err error) {
+	begin := time.Now().UnixNano()
+	beego.Info("Request url:", url)
 	req := httplib.Get(url)
 	resp := &ApiResp{}
 	err = req.ToJson(resp)
 	if err != nil {
 		return
 	}
+	end := time.Now().UnixNano()
+	beego.Info("Http response:", resp, ", cost:", end-begin)
+
+	code = resp.Head.Code
+	if resp.Head.Code != 200 {
+		err = errors.New(resp.Head.Msg)
+		return
+	}
+
 	//获取body数据
 	err = GetRespBodyData(resp, data)
 	resp.Body = &data
-	code = resp.Head.Code
+	return
+}
+
+/**
+ * 获取http请求结果，只支持post
+ * url:请求地址
+ * params：post参数
+ * data：返回数据
+ */
+func GetPostHttpRequest(url string, params map[string]string, data interface{}) (err error) {
+	defer func() {
+		if x := recover(); x != nil {
+			errStr := fmt.Sprintf("get post fail: params-%v , err-%v", params, x)
+			err = errors.New(errStr)
+		}
+	}()
+
+	begin := time.Now().UnixNano()
+	req := httplib.Post(url).SetTimeout(CONNECT_TIME_OUT, READ_WRITE_TIME_OUT).Header("Content-Type", "application/x-www-form-urlencoded")
+	beego.Info("Request url:", url, params)
+	if params != nil && len(params) > 0 {
+		for key, value := range params {
+			req.Param(key, value)
+		}
+	}
+
+	resp := &ApiResp{}
+	err = req.ToJson(resp)
+	if err != nil {
+		return
+	}
+	end := time.Now().UnixNano()
+	beego.Info("Http response:", resp, ", cost:", end-begin)
+
+	if resp.Head.Code != 200 {
+		err = errors.New(resp.Head.Msg)
+		return
+	}
+
+	if data != nil {
+		beego.Debug(resp.Body)
+		//获取body数据
+		err = GetRespBodyData(resp, data)
+		resp.Body = &data
+	}
+
+	return
+}
+
+/**
+ * 获取http请求结果，只支持post,返回结构自定义
+ * url:请求地址
+ * params：post参数
+ * data：返回数据
+ */
+func GetPostHttpRequestData(url string, params map[string]string, respData interface{}) (err error) {
+	defer func() {
+		if x := recover(); x != nil {
+			errStr := fmt.Sprintf("get post fail: params-%v , err-%v", params, x)
+			err = errors.New(errStr)
+		}
+	}()
+	begin := time.Now().UnixNano()
+	req := httplib.Post(url).SetTimeout(CONNECT_TIME_OUT, READ_WRITE_TIME_OUT).Header("Content-Type", "application/x-www-form-urlencoded")
+	beego.Info("Request url:", url, params)
+	if params != nil && len(params) > 0 {
+		for key, value := range params {
+			req.Param(key, value)
+		}
+	}
+
+	err = req.ToJson(respData)
+	if err != nil {
+		return
+	}
+	end := time.Now().UnixNano()
+	beego.Info("Http response:", respData, ", cost:", end-begin)
+
 	return
 }
 
