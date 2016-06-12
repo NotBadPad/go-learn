421f509e5056b6911fc3d82d5e552713bfb8e843
awayy1432 <awayy1432@163.com>
2016-06-12 22:56:40 +0800

    fix

--- zk/main.go
+++ zk/main.go
@@ -8,7 +8,19 @@ import (
 	zk "github.com/samuel/go-zookeeper/zk"
 )
 
-func main() {
-	fmt.Println("aaaaa")
+func test1() {
+	conn, session, err := zk.Connect([]string{"localhost:2181"}, 50000)
+	if err != nil {
+		fmt.Println(err)
+	}
 
+	defer conn.Close()
+	event := <-session
+	if event.State == zk.StateConnected {
+		conn.Create("/testadaw", []byte{1, 2, 3, 4}, 0, zk.WorldACL(zk.PermAll))
+	}
+}
+
+func main() {
+	test1()
 }
