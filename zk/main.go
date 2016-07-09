4b0baf66f79671f689dbf58fc21b64ad621118dc
awayy1432 <awayy1432@163.com>
2016-07-09 12:52:41 +0800

    fxi

--- zk/main.go
+++ zk/main.go
@@ -9,14 +9,32 @@ import (
 	"time"
 )
 
-func test1() {
-	conn, _, err := zk.Connect([]string{"localhost:2183"}, 50*time.Second)
+func getConnect(string []zkList) (conn zk.Conn) {
+	conn, _, err := zk.Connect(zkList, 10*time.Second)
 	if err != nil {
 		fmt.Println(err)
 	}
+	return
+}
+
+/**
+ * 测试连接
+ * @return
+ */
+func test1() {
+	zkList := []string{"localhost:2183"}
+	conn := getConnect(zkList)
 
 	defer conn.Close()
 	conn.Create("/testadaw", nil, zk.FlagSequence, zk.WorldACL(zk.PermAll))
+}
+
+/**
+ * 测试临时节点
+ * @return {[type]}
+ */
+func test2() {
+
 }
 
 func main() {
